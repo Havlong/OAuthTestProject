de6f9baab7cee69c481dfe5dd2f78221d66454f8
Havlong <grishin_evgenii@inbox.ru>
2022-07-13 22:20:26 +0300

    Added Action bar support feature

--- app/src/main/java/ru/havlong/test/MainActivity.kt
+++ app/src/main/java/ru/havlong/test/MainActivity.kt
@@ -2,12 +2,28 @@ package ru.havlong.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
